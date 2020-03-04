b51947eb0f877fc4cd5ce70387f35fb2deb3fb66
Ryan Kegerreis <ryankegerreis@icloud.com>
2020-03-04 07:37:12 -0500

    Tree widht

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -1,3 +1,4 @@
+
 // --- Directions
 // Given the root node of a tree, return
 // an array where each element is the width
@@ -11,6 +12,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const arr = [root, 's']
+  let counters = [0]
+
+  while (arr.length > 1) {
+    const node = arr.shift()
+
+    if (node === 's') {
+      counters.push(0)
+      arr.push('s')
+    } else {
+      arr.push(...node.children)
+      counters[counters.length - 1]++
+    }
+  }
+  return counters
+}
 
 module.exports = levelWidth;
